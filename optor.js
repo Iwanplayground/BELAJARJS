b0218f60dddd40892b4cefbae4b61b9c2179a3a0
Iwanplayground <backenddeviwan@gmail.com>
2022-08-05 16:42:43 +0700

    add a operator untill assign op

---
+++ optor.js
@@ -0,0 +1,13 @@
+var a = 3;
+var b = 10;
+// mau aritmatika? EZ
+var c = a + ((b * b * b) % 2);
+console.log(c);
+// jangan pula dikasi quotes nanti jadi string dia
+var x = "10000" + "898";
+console.log(x);
+
+// operator assignment itu spt +=,*=,**=
+var jumlahView = 11;
+jumlahView **= 3;
+console.log(jumlahView);
