9afe82cabafe8d7afd727245fcadecd05e8958f9
Iwanplayground <backenddeviwan@gmail.com>
2022-08-05 16:00:53 +0700

    add a variable and dtype in var.js

---
+++ var.js
@@ -0,0 +1,20 @@
+var siteName = "BukaKedai";
+var url = "BukaKedai.co.id";
+var visitorCount = 534;
+
+// nampilin isi variabelnya
+console.log("nama website dari company kami adalah: " + siteName);
+
+// mau ubah domain webnya misal
+console.log("===awal===");
+console.log("nama website dari company kami adalah: " + url);
+url = "BukaKedai.com";
+console.log("===akhir===");
+console.log("nama website dari company kami adalah: " + url);
+
+// kalo mau hapus tinggal di assign jangan lupa awalnya kasi delete namaVar
+
+// kenal tipe data, ini umum banget sih ya istilahnya kita cuma reviw aja masing2
+var name = "Iwan Kuswanto";
+var age = 40;
+console.log(typeof age);
