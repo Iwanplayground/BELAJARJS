c32bff860007385fc9081909c0b7d7d8891ec576
Iwanplayground <backenddeviwan@gmail.com>
2022-08-06 11:09:32 +0700

    menambahkan array.js ke repo

--- array.js
+++ array.js
@@ -6,3 +6,15 @@ console.log("");
 for (let i = 0; i < products.length; i++) {
   console.log(i + ". " + products[i]);
 }
+
+// cara nambahin/ngubah data ke dalam array gmna?
+var buah = ["Apel", "Jeruk", "Manggis"];
+// mirip sm py bet, ni kalo NGUBAH
+buah[2] = "Kedondong";
+console.log(buah);
+// kalo nambah gmna? ya bisa dilihat indexnya kalo itu cuma 2 ya kasi ke-3 nya
+buah[3] = "Jeruk";
+console.log(buah);
+// kalo ndatau jumlah indexnya? yaudah pake push aja
+buah.push("Stroberi", "Semangka", "Melon");
+console.log(buah);
