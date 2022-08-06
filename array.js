1ad1768127a83c5b20a98a05fcd018b0d9c1736d
Iwanplayground <backenddeviwan@gmail.com>
2022-08-06 10:06:29 +0700

    menambahkan sdikit elemen di array.js

---
+++ array.js
@@ -0,0 +1,8 @@
+var products = ["Flashdisk", "SDD", "Monitor"];
+// ngambil data di array
+console.log(products[1]);
+console.log("");
+// mau ambil smuanya gimana ya tinggal kita looping
+for (let i = 0; i < products.length; i++) {
+  console.log(i + ". " + products[i]);
+}
